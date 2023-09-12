a49b9153847c87851d612e1545beade121a6d57d
kyle <75395106+ehre9305@users.noreply.github.com>
2023-09-12 17:09:14 -0500

    fix controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,12 +2,13 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.DriveTrainSubsystem;
 import frc.robot.commands.Drive;
+import frc.robot.subsystems.DriveTrainSubsystem;
 
 public class RobotContainer {
-  //All instances of commands, subssytems, and controllers should be defined here
-  //It's not a hard rule, but if you follow this guideline your code will be more organized
+  // All instances of commands, subssytems, and controllers should be defined here
+  // It's not a hard rule, but if you follow this guideline your code will be more
+  // organized
   private final XboxController driverController = new XboxController(0);
   private final DriveTrainSubsystem driveTrain = new DriveTrainSubsystem();
 
@@ -18,32 +19,40 @@ public class RobotContainer {
     configureDefaultCommands();
   }
 
-  //This is where we associate buttons with commands
-  //Usually goes like:
-  //When this button is pressed, run this command
-  //Once the button is released, end the command
-  //Right now we only have the Drive command, which we don't ever want to end
-  //A command that we want to always be running by default is a default command
-  //I've created a method called configureDefaultCommands that configure the default command
-  private void configureButtonBindings() {}
+  // This is where we associate buttons with commands
+  // Usually goes like:
+  // When this button is pressed, run this command
+  // Once the button is released, end the command
+  // Right now we only have the Drive command, which we don't ever want to end
+  // A command that we want to always be running by default is a default command
+  // I've created a method called configureDefaultCommands that configure the
+  // default command
+  private void configureButtonBindings() {
+  }
 
-  //This is where the default commands are defined
+  // This is where the default commands are defined
   private void configureDefaultCommands() {
-    //Each subsystem can get a default command, which is the command it runs unless another command
-    //is called that uses the subsystem
-    //Right now we only have the driveTrain subsystem
-    //We are passing in a new Drive command as an argument in the setDefaultCommand method
-    //The first argument is the driveTrain, the second and third arguments are double suppliers
-    //for forward (getLeftY) and rotation (getLeftX)
-    //The notation () -> body
-    //is called lambda notation, it's a quick way of creating a function
-    //Suppliers are functions, lambda notation let's us write functions using little code
-    //The arguments to the function are inside the (), right now we don't have any arguments, 
-    //so () is empty
-    //-> separate the () and body. It tells the computer that whatever comes after ->
-    //is the body of the function. The body is what the function definition
-    driveTrain.setDefaultCommand(new Drive(driveTrain, () -> driverController.getLeftY(), 
-    () -> driverController.getLeftX()));
+    // Each subsystem can get a default command, which is the command it runs unless
+    // another command
+    // is called that uses the subsystem
+    // Right now we only have the driveTrain subsystem
+    // We are passing in a new Drive command as an argument in the setDefaultCommand
+    // method
+    // The first argument is the driveTrain, the second and third arguments are
+    // double suppliers
+    // for forward (getLeftY) and rotation (getLeftX)
+    // The notation () -> body
+    // is called lambda notation, it's a quick way of creating a function
+    // Suppliers are functions, lambda notation let's us write functions using
+    // little code
+    // The arguments to the function are inside the (), right now we don't have any
+    // arguments,
+    // so () is empty
+    // -> separate the () and body. It tells the computer that whatever comes after
+    // ->
+    // is the body of the function. The body is what the function definition
+    driveTrain.setDefaultCommand(new Drive(driveTrain, () -> -driverController.getLeftY(),
+        () -> -driverController.getRightX()));
   }
 
   public Command getAutonomousCommand() {
